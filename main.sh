c355e2ffd88964b95d44fda09907cadaa2a0fb99
fr3e1 <fr3e.acc0unt@gmail.com>
2024-05-08 13:29:30 -0600

    updated main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-
+b
 #/bin/bash
 
 select-menu() {
@@ -8,7 +8,7 @@ select-menu() {
 	echo "[3]"
 }
 
-select-programs() {
+selectPrograms() {
 	echo "type out what program you wish to use:"
 	echo
 	ls programs/
@@ -22,26 +22,51 @@ select-programs() {
 	fi
 }
 
-create-program() {
+createProgram() {
 	clear
 	echo "enter a name"
 	read -p ">" choice
 	echo
-	if [! -d $choice ];then 
+	if [! -d programs/$choice ];then 
 		echo "making directory"
 		mkdir programs/$choice
 	else
 		echo "directory already exists, exiting"
 	fi
+	if [ -d programs/$choice ]; then
+		echo "directory made"
+	else
+		echo "directory was not made and/or missing"
+	fi
+	echo > programs/$choice/main.sh <<EOF
+#!/bin/bash
+#write your code here
 
+start() {
 
 }
 
+EOF
 
-bash "install-dependancies.sh"
-clear
-echo "FR3E'S AP COMP SCI 20-1 PROJECT" 
-echo
-echo "select an option:"
-echo "[1] - select program(s)"
-echo "[2] - create a program"
+echo "main.sh created"
+}
+
+while true; do
+
+	bash "install-dependancies.sh"
+	clear
+	echo "FR3E'S AP COMP SCI 20-1 PROJECT" 
+	echo
+	echo "select an option:"
+	echo "[1] - select program(s)"
+	echo "[2] - create a program"
+	echo
+	echo "[0] - exit"
+	read -p ">" choice2
+	case $choice2 in
+		1)selectProgram ;;
+		2)createProgram ;;
+		0) echo "Exiting..."; exit 0 ;;
+	esac
+done
+
